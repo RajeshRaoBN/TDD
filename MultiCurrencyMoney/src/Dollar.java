5c5f542988eb650fdab0e4c8b0ed46f60259b836
Rajesh <mailrbn@gmail.com>
2018-05-14 09:50:13 +1000

    inter14052018

---
+++ MultiCurrencyMoney/src/Dollar.java
@@ -0,0 +1,8 @@
+public class Dollar {
+    int amount;
+    int Dollar(int amount) {
+        return amount;
+    }
+    void times(int multiplier) {
+    }
+}
---
+++ MultiCurrencyMoney/src/TestJunit2.java
@@ -0,0 +1,2 @@
+public class TestJunit2 {
+}
